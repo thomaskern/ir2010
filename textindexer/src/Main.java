3dd847763be05a33f052e09bf960a32111c1c112
Thomas Kern <thomaskern@noname.(none)>
2010-03-21 18:35:32 +0100

    gitignore adjusted

---
+++ textindexer/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: thomaskern
+ * Date: Mar 21, 2010
+ * Time: 6:34:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
