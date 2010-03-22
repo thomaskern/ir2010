97c44843152458a59aeea8cc0ac146b7351ac2c5
Thomas Kern <thomaskern@noname.(none)>
2010-03-22 13:30:53 +0100

    angabe added

---
+++ textindexer/src/Reader.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: thomaskern
+ * Date: Mar 22, 2010
+ * Time: 1:30:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Reader {
+
+    
+
+}
